b3ff6965d76abab0e78c976c558bc5b00287971c
Shiv Shankar Singh <malviyanshiv@gmail.com>
2020-05-29 22:09:06 +0530

    cleaning messageTwo data on error

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,8 @@ const messageTwo = document.querySelector("#message-2");
 weatherForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const location = search.value;
-    messageOne.textContent = "Loading!!!";
+    messageOne.textContent = "Loading...";
+    messageTwo.textContent = "";
 
     fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
